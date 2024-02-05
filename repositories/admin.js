b743d9782fb8508a98211e3d726a55bbaf4fe41c
jason <jason.tseng@adgeek.com.tw>
2024-02-05 12:23:08 +0800

    Fix token should take care by front end

--- repositories/admin.js
+++ repositories/admin.js
@@ -6,24 +6,21 @@ const admin = express();
 const adminTokenURL = 'https://dev-btzqcfy7fq2uv7xm.jp.auth0.com/oauth/token'
 const headers= {'content-type': 'application/json'};
 Object.freeze(headers)
-let token = ''
-admin.get("/admin-login", async (req, res, next) => {
+admin.post("/admin-login", async (req, res, next) => {
     const resp = await axios.post(adminTokenURL,
         JSON.stringify(authConfig),
         {headers},
     );
 
-    token = resp.data.access_token
-    // res.send("ok")
     res.send(resp.data)
     next()
 })
 admin.get("/user-list", async (req, res, next) => {
-    if (token !== '') {
+    if (req.headers.authorization !== '') {
         const resp = await axios.get('https://dev-btzqcfy7fq2uv7xm.jp.auth0.com/api/v2/users',
             {headers: {
                 ...headers,
-                'Authorization': `Bearer ${token}`
+                Authorization: req.headers.authorization,
             }},
         );
 

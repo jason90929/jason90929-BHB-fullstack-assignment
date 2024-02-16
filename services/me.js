b272e6c6af080cd6e898ff8edf838d576b707e4d
jason <jason.tseng@adgeek.com.tw>
2024-02-16 11:42:33 +0800

    Refactor removing async/await replace it by then/catch

--- services/me.js
+++ services/me.js
@@ -4,62 +4,61 @@ const authConfig = require("../secret/management_config.json")
 const {defaultHeaders} = require("../constants");
 const {jsonParser, requiresAuth, adminLogin} = require("../middlewares");
 const me = express();
-me.get("/me", requiresAuth(), adminLogin, async (req, res, next) => {
-    try {
-        const resp = await axios.get(`${authConfig.audience}users/${req.oidc.user.sub}`,
-            {
-                headers: {
-                    ...defaultHeaders,
-                    Authorization: req.headers.authorization,
-                }
-            },
-        )
+me.get("/me", requiresAuth(), adminLogin, (req, res, next) => {
+    axios.get(`${authConfig.audience}users/${req.oidc.user.sub}`,
+        {
+            headers: {
+                ...defaultHeaders,
+                Authorization: req.headers.authorization,
+            }
+        },
+    ).then(function (resp) {
         res.send(resp.data)
-    } catch (err) {
+    }).catch(function (err) {
+        console.log("get user failed", err)
         res.send(`error: ${err.message}`)
-    }
+    })
 })
 
-me.patch("/me", requiresAuth(), adminLogin, jsonParser, async (req, res, next) => {
-    try {
-        const resp = await axios.patch(`${authConfig.audience}users/${req.oidc.user.sub}`, {
-                name: req.body.name,
-                password: req.body.password,
-                // Add additional fields if needed
-            },
-            {
-                headers: {
-                    ...defaultHeaders,
-                    Authorization: req.headers.authorization,
-                }
-            }
-        )
+me.patch("/me", requiresAuth(), adminLogin, jsonParser, (req, res, next) => {
+    axios.patch(`${authConfig.audience}users/${req.oidc.user.sub}`, {
+        name: req.body.name,
+        password: req.body.password,
+        // email_verified: req.body.email_verified,
+        // Add additional fields if needed
+    },
+    {
+        headers: {
+            ...defaultHeaders,
+            Authorization: req.headers.authorization,
+        }
+    }).then(function (resp) {
         res.send(resp.data)
-    } catch (err) {
+    }).catch(function (err) {
+        console.log("patch user failed", err)
         res.send(`error: ${err.message}`)
-    }
+    })
 })
 
-me.post("/me/resend-email-verification", requiresAuth(), adminLogin, jsonParser, async (req, res, next) => {
-    try {
-        const resp = await axios.post(`${authConfig.audience}jobs/verification-email`, {
-                user_id: req.oidc.user.sub,
-                // identity: {
-                //     user_id: req.params.id,
-                //     provider: ""
-                // },
-            },
-            {
-                headers: {
-                    ...defaultHeaders,
-                    Authorization: req.headers.authorization,
-                }
-            }
-        )
+me.post("/me/resend-email-verification", requiresAuth(), adminLogin, jsonParser, (req, res, next) => {
+    axios.post(`${authConfig.audience}jobs/verification-email`, {
+        user_id: req.oidc.user.sub,
+        // identity: {
+        //     user_id: req.params.id,
+        //     provider: ""
+        // },
+    },
+    {
+        headers: {
+            ...defaultHeaders,
+            Authorization: req.headers.authorization,
+        }
+    }).then(function (resp) {
         res.send(resp.data)
-    } catch (err) {
+    }).catch(function (err) {
+        console.log("patch user failed", err)
         res.send(`error: ${err.message}`)
-    }
+    })
 })
 
 module.exports = me
--- services/users.js
+++ services/users.js
@@ -4,20 +4,20 @@ const authConfig = require("../secret/management_config.json")
 const {defaultHeaders} = require("../constants");
 const {adminLogin} = require("../middlewares");
 const users = express();
-users.get("/users", adminLogin, async (req, res, next) => {
-    try {
-        const resp = await axios.get(`${authConfig.audience}users`,
-            {
-                headers: {
-                    ...defaultHeaders,
-                    Authorization: req.headers.authorization,
-                }
-            },
-        )
+users.get("/users", adminLogin, (req, res, next) => {
+    axios.get(`${authConfig.audience}users`,
+        {
+            headers: {
+                ...defaultHeaders,
+                Authorization: req.headers.authorization,
+            }
+        },
+    ).then(function (resp) {
         res.send(resp.data)
-    } catch (err) {
-        res.send(err)
-    }
+    }).catch(function (err) {
+        console.log("get users failed", err)
+        res.send(`error: ${err.message}`)
+    })
 })
 
 module.exports = users
